8569148159beb6d5174022f61dfb6c8bb4d6e1a2
version.py script <nate@securitymetrics.com>
2011-08-02 16:45:08 -0600

    added command li ne options

--- test.py
+++ test.py
@@ -1,9 +1,32 @@
-#!/usr/bin/python2.4
-import imagemodel
+#!/usr/bin/env python
 
-def main():
-    i = imagemodel.ImageModel(['/Users/dave/Pictures/Backgrounds'])
-    i.run()
+import imagemodel, sys
+from twisted.python import usage
+
+
+class StartOptions(usage.Options):
+    def getSynopsis(self):        
+        return """Usage: %s <options>"""
+
+    optParameters = [
+            ('width', 'w', 1024, 'Width of screen'),
+            ('height', 'h', 768, 'Height of screen'),
+            ('location', 'i', None, 'Location of pictures'),
+            ]
+
+def parseOptions(argv):
+    opt = StartOptions()
+    try:
+        opt.parseOptions(argv[1:])
+    except UsageError, e:
+        raise SystemExit(str(e))
+    return opt
+
+
+def main(argv):
+    opt = parseOptions(argv)
+    #i = imagemodel.ImageModel(['/Users/dave/Pictures/Backgrounds'])
+    #i.run()
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
